27c63acc07fd44adcedfa65346b448739b11c60b
Rohit Joshi <link2j.roy@gmail.com>
2018-09-04 16:18:28 +0100

    Reorganize folders and A working ECS-Cloudformation demo

--- app.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra'
-require 'sinatra/base'
-
-class App < Sinatra::Base
-  get '/' do
-    "Hello from sinatra! The time is #{ Time.now } on #{ `hostname` }!"
-  end
-end
--- config.ru
+++
@@ -1,4 +0,0 @@
-$:.unshift(File.dirname(__FILE__))
-require 'app'
-
-Rack::Handler.default.run(App, :Port => 9494)
